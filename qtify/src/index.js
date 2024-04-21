d00895a5bc4daac77cd598f60908cef613546f88
Bibhushan <bibhus47@in.ibm.com>
2024-04-22 00:24:24 +0530

    Removed webfontinstaller package

--- qtify/src/index.js
+++ qtify/src/index.js
@@ -3,13 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import WebFont from "webfontloader";
-
-WebFont.load({
-  google: {
-    families: ['Poppins:300,400,700', 'Roboto']
-  }
-});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
