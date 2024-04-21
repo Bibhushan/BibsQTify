396b1c5e506a38e7f7660cf2fc487f95724afa49
Bibhushan <bibhus47@in.ibm.com>
2024-04-21 23:21:58 +0530

    Added Navbar and Cleaned Up

--- qtify/src/components/QtifyMainPage.js
+++ qtify/src/components/QtifyMainPage.js
@@ -1,12 +1,9 @@
-// import AppBar from "@mui/material/AppBar";
-import PrimarySearchAppBar from "./AppBar";
 import QtifyNavBar from "./QtifyNavBar";
 
 export default function QtifyMainPage(){
 
     return (
         <div>
-            <PrimarySearchAppBar/>
             <QtifyNavBar />
         </div>
     )
